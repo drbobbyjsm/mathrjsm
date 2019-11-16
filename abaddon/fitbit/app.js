ec9c94c149414db801be5e16a62c76ce58b5200f
rjsmcdona <44184017+rjsmcdona@users.noreply.github.com>
2019-11-15 20:21:51 -0500

    asdf

--- abaddon/fitbit/app.js
+++ abaddon/fitbit/app.js
@@ -6,3 +6,5 @@ var access_token = url.split("#")[1].split("=")[1].split("&")[0];
 var userId = url.split("#")[1].split("=")[2].split("&")[0]; 
 console.log(access_token); 
 console.log(userId);
+document.getElementById("access_token").innerHTML = access_token
+document.getElementById("userId").innerHTML = userId
